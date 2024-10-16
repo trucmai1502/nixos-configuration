b2e771ab559be1adad4408a118617b2e2038008e
trucmai1502 <trucmai.tomorrow@gmail.com>
2024-10-16 08:29:22 +0700

    update stylix

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -10,7 +10,7 @@
 
   stylix = {
     enable = true;
-    image = /home/${outputs.username}/nix-config/hatsukoinonioi.png;
+    image = /home/${outputs.username}/nix-config/122315819.png;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/${outputs.theme}.yaml";
 
